ff9805c90915d9ee65cd0d49ab0c41a0f1d445e8
ALowton <ALowton@QA.com>
2022-09-06 16:35:00 +0100

    AL: more controller

--- src/main/java/com/qa/main/controllers/CustomerController.java
+++ src/main/java/com/qa/main/controllers/CustomerController.java
@@ -3,8 +3,11 @@ package com.qa.main.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.qa.main.entities.Customer;
@@ -15,12 +18,6 @@ public class CustomerController {
 	// Temporary Storage, until we implement the real database
 	private List<Customer> customers = new ArrayList<>();
 	
-	// Get Requests (READ)
-	@GetMapping("/getAll")
-	public List<Customer> getAll() {
-		return customers;
-	}
-	
 	// Post Requests (CREATE)
 	@PostMapping("/create")
 	public Customer create(@RequestBody Customer input) {
@@ -29,9 +26,33 @@ public class CustomerController {
 		return customers.get(customers.size() - 1);
 	}
 	
+	// Get Requests (READ)
+	@GetMapping("/getAll")
+	public List<Customer> getAll() {
+		return customers;
+	}
+	
+	@GetMapping("/getById/{id}")
+	public Customer getById(@PathVariable int id) {
+		return customers.get(id);
+	}
+	
 	// Put Requests (UPDATE)
+	@PutMapping("/update/{id}")
+	public Customer update(@PathVariable int id, @RequestBody Customer input) {
+		// Remove original user
+		customers.remove(id);
+		
+		// Add the updated user in the same position
+		customers.add(id, input);
+		
+		return customers.get(id);
+		
+	}
 	
-	
-	// Delete Requests (DELETE)
-	
+//	// Delete Requests (DELETE)
+//	@DeleteMapping("/delete")
+//	public Customer delete() {
+//		
+//	}
 }
