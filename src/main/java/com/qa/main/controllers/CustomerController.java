91d964fa879e2012c0b36d494e5f4cbd69e200ce
ALowton <ALowton@QA.com>
2022-09-08 11:06:35 +0100

    AL: res entities and controller mapping

--- src/main/java/com/qa/main/controllers/CustomerController.java
+++ src/main/java/com/qa/main/controllers/CustomerController.java
@@ -1,17 +1,22 @@
 package com.qa.main.controllers;
 
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.qa.main.entities.Customer;
 import com.qa.main.services.CustomerService;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
 	
 	private CustomerService service;
@@ -23,8 +28,8 @@ public class CustomerController {
 
 	// Post Requests (CREATE)
 	@PostMapping("/create")
-	public Customer create(@RequestBody Customer input) {
-		return service.create(input);
+	public ResponseEntity<Customer> create(@RequestBody Customer input) {
+		return new ResponseEntity<Customer>(service.create(input), HttpStatus.CREATED);
 	}
 	
 	// Get Requests (READ)
